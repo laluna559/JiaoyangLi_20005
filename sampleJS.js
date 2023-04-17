f3b590c507f491d2a06bf5d456672ced2fd757d5
laluna559 <129711177+laluna559@users.noreply.github.com>
2023-04-17 02:46:14 -0500

    Update sampleJS.js

--- sampleJS.js
+++ sampleJS.js
@@ -11,3 +11,29 @@ function hideElement() {
     var secondParagraph = document.getElementById("second");
     secondParagraph.style.display = "none";
 }
+function showAlert() {
+    alert("Hello World!");
+}
+function addLinks(links) {
+    var ul = document.querySelector('ul');
+    for (var i = 0; i < links.length; i++) {
+      var li = document.createElement('li');
+      var a = document.createElement('a');
+      a.setAttribute('href', links[i].url);
+      a.textContent = links[i].text;
+      li.appendChild(a);
+      ul.appendChild(li);
+    }
+  }
+  
+  var links = [
+    {url: 'https://example.com', text: 'Link 1'},
+    {url: 'https://example.com', text: 'Link 2'},
+    {url: 'https://example.com', text: 'Link 3'},
+  ];
+  
+  addLinks(links);
+function parseArray(arr) {
+    return arr.sort();
+}
+  
